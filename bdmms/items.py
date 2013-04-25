edf85c0c50291dcf62dc3d5547adf74a881ac940
shuji <bh2@qq.com>
2013-04-25 15:13:58 +0800

    添加忽略规则文件

--- bdmms/items.py
+++ bdmms/items.py
@@ -1,11 +1,5 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# http://doc.scrapy.org/topics/items.html
-
 from scrapy.item import Item, Field
 
+
 class BdmmsItem(Item):
-    # define the fields for your item here like:
-    # name = Field()
     pass
